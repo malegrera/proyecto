59c5707fedfdf9dd389454b380cb5019cdb4f8e5
malegrera <malegrera@uoc.edu>
2021-11-12 08:34:20 +0100

    cambios

--- dist/index.4e40b6e8.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".fecha").textContent=(new Date).toLocaleDateString("es-ES",{weekday:"long",year:"numeric",month:"long",day:"numeric"});
-//# sourceMappingURL=index.4e40b6e8.js.map
--- dist/index.e150cc87.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".fecha").textContent=(new Date).toLocaleDateString("es-ES",{weekday:"long",year:"numeric",month:"long",day:"numeric"});
-//# sourceMappingURL=index.e150cc87.js.map
