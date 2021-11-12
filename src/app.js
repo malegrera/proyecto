7c3877bd57b66f4a3c57bdf0689bf0cad81ba704
malegrera <malegrera@uoc.edu>
2021-11-12 11:36:05 +0100

    publicación

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
 
+import dayjs from 'dayjs';
+console.log(dayjs().format());
 document.querySelector(".fecha").textContent = new Date().toLocaleDateString('es-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
-
+document.querySelector(".inicio1").textContent = dayjs().add(1,'month').format('DD/MM/YYYY');
+document.querySelector(".final1").textContent = dayjs().add(1, 'month').add(7, 'day').format('DD/MM/YYYY');
